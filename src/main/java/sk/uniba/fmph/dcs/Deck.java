461ad851af9405fa4b29c0f9a16201c3c629f402
tanahusarova <83506375+tanahusarova@users.noreply.github.com>
2021-11-07 09:33:14 +0100

    Update Deck.java

--- src/main/java/sk/uniba/fmph/dcs/Deck.java
+++ src/main/java/sk/uniba/fmph/dcs/Deck.java
@@ -15,21 +15,24 @@ public class Deck {
         ArrayList<CardInterface> cardsToDraw = new ArrayList<>();
         int size = deckCards.size();
         if (size < count) {
+            /* tuto ked napriklad chces tahat 5 kariet ale v baliku mas uz iba 4 (takze podmienka prejde), by si najskor 
+            mala vytiahnut tie 4 co boli povodne v decku. Ale pri deckCards.clear ich zahodis, to clear by som dala prec. */
+           
             ArrayList<CardInterface> newDeckCards = new ArrayList<>(discardPile.shuffle());
             newDeckCards.addAll(deckCards);
-            deckCards.clear();
+            deckCards.clear(); //len toto
             deckCards.addAll(newDeckCards);
         }
         size = deckCards.size();
         for (int i = 0; i < count; i++) {
-            CardInterface card = deckCards.get(size - 1 - i);
+            CardInterface card = deckCards.get(size - 1 - i); //si si mohla dat do decku daku pomocnu ze vrat hornu kartu, by si sa potom nemusela trapit s tymto
             cardsToDraw.add(card);
             deckCards.remove(size - 1 - i);
         }
         return cardsToDraw;
     }
 
-    public int getPoints() {
+    public int getPoints() { //nerobi sa toto az v play triede?
         int points = 0;
         for (CardInterface card : deckCards) {
             points += card.cardType().getPoints();
